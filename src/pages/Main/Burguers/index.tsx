128a222ccf82ab3260968cacb7200eba0fd720f0
IceRed0991 <wernergutschow@hotmail.com>
2023-08-14 17:22:00 -0300

    Products on feed

--- src/pages/Main/Burguers/index.tsx
+++ src/pages/Main/Burguers/index.tsx
@@ -3,6 +3,25 @@ import { SnackTitle } from '../../../components/SnackTitle';
 
 
 export default function Burguers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burguer',
+      name: 'Mega',
+      description: 'O artesanal tamanho familia recheado com três carnes suculentas queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/R72rW81.jpeg',
+    },
+    {
+      id: 2,
+      snack: 'burguer',
+      name: 'Extra bacon',
+      description: 'Criado para os amantes de bancon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/uRVpW7J.jpeg',
+    },
+  ]
+
   return (
     <>
       <Head title="Hambúrguer" description="Nossos Melhores Burguers" />
