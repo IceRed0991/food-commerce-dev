027d92bcf1896ebdb4eaee737e2f6891d38949db
IceRed0991 <wernergutschow@hotmail.com>
2023-08-30 20:24:53 -0300

    update

--- src/pages/MyCart/Table/TableDesktop/index.tsx
+++ src/pages/MyCart/Table/TableDesktop/index.tsx
@@ -2,6 +2,9 @@ import { Container } from "./styles";
 import { useCart } from "../../../../hooks/useCart";
 import currencyFormat from "../../../../helpers/currencyFormat";
 
+import minusImg from '../../../../assets/circle-minus.svg'
+import plusImg from '../../../../assets/circle-plus.svg'
+
 export function TableDesktop() {
   const { cart } = useCart()
 
@@ -29,7 +32,15 @@ export function TableDesktop() {
                   <span>{currencyFormat(item.price)}</span>
                 </td>
                 <td>
-                  {item.quantity}
+                  <div>
+                    <button type='button' onClick={() => console.log('-1', item)}>
+                      <img src={minusImg} alt="Remover Quantidade" />
+                    </button>
+                    <span>{`${item.quantity}`.padStart(2, '0')}</span>
+                    <button type='button' onClick={() => console.log('+1', item)}>
+                      <img src={plusImg} alt="Adicionar Quantidade" />
+                    </button>
+                  </div>
                 </td>
                 <td>
                   <h5>{currencyFormat(item.subtotal)}</h5>
--- src/pages/MyCart/Table/TableDesktop/styles.ts
+++ src/pages/MyCart/Table/TableDesktop/styles.ts
@@ -38,6 +38,26 @@ border-radius: 8px;
         font-weight: 700;
         font-size: 1.5rem;
       }
+
+      div{
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        button{
+          background: none;
+          flex: 0;
+          border: none;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          img{
+            width: 1.50rem;
+          }
+        }
+      }
+
+
       /*Quantidade*/
       h5{
         font-weight: 700;
